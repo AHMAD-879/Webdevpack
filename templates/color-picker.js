d6ce659ab426789fc06af8553eeeff534bcaad85
Ahmad Badghaish <ahmad.badghaish1@gmail.com>
2022-06-04 19:00:35 +0300

    Async script. Does not update hex value.

--- templates/color-picker.js
+++ templates/color-picker.js
@@ -2,6 +2,5 @@ const cp = document.getElementById("colorPicker");
 const label = document.getElementById("colorLabel");
 
 cp.addEventListener("change", function() {
-    alert("Henlo");
-    label.innerHTML = "Eg";
+    label.innerHTML = "Hex: " + cp.getAttribute("value");
 });
